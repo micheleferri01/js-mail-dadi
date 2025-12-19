340a4547c3fe4d4e880514895c952e93fb4d9a89
micheleferri01 <micheleferri095@gmail.com>
2025-12-19 15:17:16 +0100

    Pseudo codice (Gioco dei dadi)

--- Gioco-dei-dadi/dadi.js
+++ Gioco-dei-dadi/dadi.js
@@ -2,3 +2,20 @@
 // Richiesta
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// # Pseudo Codice
+
+// ## Raccolta dati
+// Dichiaro il numero massimo
+// Dichiaro il numero minimo
+
+// ## Elaborazione
+// Genero numero randomico per l'utente
+// Genero numero randomico per il computer
+// Controllo chi ha il punteggio più alto
+
+// ## Output
+// SE (numero utente > numero computer)
+// Ha vinto l'utente
+// ALTRIMENTI
+// Ha vinto il computer
