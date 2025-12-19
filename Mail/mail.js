5a051cc1b2672709621a933787d94679705df46e
micheleferri01 <micheleferri095@gmail.com>
2025-12-19 13:40:52 +0100

    pseudo codice (mail)

--- Mail/mail.js
+++ Mail/mail.js
@@ -6,5 +6,21 @@
 // stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
 // Non è consentito usare nessun metodo proprio degli array(come includes, per esempio).
 // Si può fare ? Certo che si basta ragionare un po’.
-// Nota: 
+// Nota:
 // Non è necessario provvedere alla validazione delle email
+
+// # Pseudo codice
+
+// # Raccolta dati
+// Dichiaro un array con tutte le mail di chi può accedere alla festa
+// chiedo all'utente la sua mail
+
+// ## Elaborazione
+// Scorro la lista delle mail
+// controllo che la mail dell'utente sia nella lista
+
+// ## Output
+// SE (la mail dell'utente è nella lista)
+// scrivo che l'utente è invitato alla festa
+// ALTRIMENTI
+// scrivo che l'utente non è invitato alla festa
